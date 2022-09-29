cbfdc6753ad592de2ccf4da66d589c7920cbe0f2
C00MAR <moureau.marc78@gmail.com>
2022-09-29 09:44:52 +0200

    :sparkles: Mise en Place de la Nav Bar avec animation :sparkles:

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const svg = document.querySelectorAll("#textsvg path");
 for (let i = 0; i < svg.length; i++){
     console.log(`Letter ${i} is ${svg[i].getTotalLength()}`)
 }
+
+$(".picto").on("click", function(){
+    $(".picto").toggleClass("close-btn");
+    $(".nav ").toggleClass("open-menu");
+    $("#textsvg").toggleClass("blur");
+});
